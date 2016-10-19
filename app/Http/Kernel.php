c9ecdee0aa92dbe18294fa47c873b027ad9a0865
Okubanjo Oluwafunsho <info@devfunsho.com>
2016-10-19 16:43:29 +0100

    Remove CORS implementation by Emeka

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,6 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             
             \App\Http\Middleware\AuthChecker::class,
-            \App\Http\Middleware\CORS::class,
         ],
 
         'api' => [
