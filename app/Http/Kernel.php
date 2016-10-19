c7d9f226d634cbeba8c5da87b1794b308907efb3
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2016-10-19 15:51:51 +0100

    add cores

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             
             \App\Http\Middleware\AuthChecker::class,
+            \App\Http\Middleware\CORS::class,
         ],
 
         'api' => [
